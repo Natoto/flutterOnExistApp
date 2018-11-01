5502050c8ec8dc103b94e2e61df209c54b7b44a6
huangbo <huangbo@yy.com>
2018-11-01 20:23:28 +0800

    改造engine添加多个工程
    改造engine添加多个flutter工程，实现插件化

---
+++ flutterOnExistApp/FlutterTesterViewController.m
@@ -0,0 +1,173 @@
+//
+//  ViewController.m
+//  flutterOnExistApp
+//
+//  Created by boob on 2018/10/9.
+//  Copyright © 2018年 YY.Inc. All rights reserved.
+//
+
+#import "FlutterTesterViewController.h"
+#import <Flutter/Flutter.h>
+#import "GeneratedPluginRegistrant.h"
+#import "SMDNetWork.h"
+#import <SSZipArchive/SSZipArchive.h>
+
+//#import <FLEX/FLEX.h>
+@interface FlutterTesterViewController ()
+@property (nonatomic, weak) FlutterViewController * ctr;
+@end
+
+@implementation FlutterTesterViewController
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+    [self createButton:@"加载boundle资源" frame:CGRectMake(80.0, 210.0, 160.0, 40.0) action:@selector(handleBoundleResource )];
+
+    [self createButton:@"加载doc资源" frame:CGRectMake(80.0, 260.0, 160.0, 40.0) action:@selector(handleDocumentButtonAction:)];
+
+    [self createButton:@"加载网络资源" frame:CGRectMake(80.0, 310.0, 160.0, 40.0) action:@selector(handleNetWorkResource:)];
+
+    NSArray * paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString * path = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"flutter_assets"] ;
+    NSLog(@"path: %@",path);
+    
+}
+
+-(void)handleNetWorkResource:(UIButton *)button{
+
+    //https://file.io/L6Ogcg
+    button.enabled = NO;
+    __weak __typeof(self)weakSelf = self;
+    void (^unzipCompleteBlock)(NSString * tempZipDir) = ^(NSString *tempZipDir) {
+           NSString * unZipDir  = [tempZipDir stringByAppendingPathComponent:@"flutter_assets"];
+              NSURL * url = [NSURL URLWithString:unZipDir ];
+            FlutterDartProject * dart = [[FlutterDartProject alloc] initWithFlutterAssets:url dartMain:nil packages:nil];
+            FlutterViewController* flutterViewController = [[FlutterViewController alloc] initWithProject:dart nibName:nil bundle:nil];
+            [GeneratedPluginRegistrant registerWithRegistry:flutterViewController];
+        
+            [weakSelf addBackButton:flutterViewController];
+ 
+             [flutterViewController setInitialRoute:@"route1"];
+            [weakSelf presentViewController:flutterViewController animated:YES completion:nil];
+        };
+    
+    [self get_remote_flutterassets:@"http://phe7wo0ui.bkt.clouddn.com/flutter_assets.zip" progress:^(float progress) {
+        button.enabled = NO;
+        [button setTitle:[NSString stringWithFormat:@"%.2f%%",100*progress] forState:UIControlStateDisabled];
+    } complete:^(NSString *localpath) {
+        button.enabled = YES;
+        unzipCompleteBlock(localpath);
+    }];
+  
+}
+
+/** 
+* 加载boundle资源 
+*/
+- (void)handleBoundleResource {
+    
+//    NSArray * paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString * path = [[NSBundle mainBundle] pathForResource:@"flutter_assets" ofType:@""];// [[paths objectAtIndex:0] stringByAppendingPathComponent:@"flutter_assets_2"] ;
+    NSURL * url = [NSURL URLWithString:path];
+    FlutterDartProject * dart = [[FlutterDartProject alloc] initWithFlutterAssets:url dartMain:nil packages:nil];
+    FlutterViewController* flutterViewController = [[FlutterViewController alloc] initWithProject:dart nibName:nil bundle:nil];
+    [GeneratedPluginRegistrant registerWithRegistry:flutterViewController];
+    
+    [self addBackButton:flutterViewController];
+
+//    [self.navigationController pushViewController:flutterViewController animated:YES];
+     [flutterViewController setInitialRoute:@"route1"];
+    [self presentViewController:flutterViewController animated:YES completion:nil];
+    
+}
+
+- (void)handleDocumentButtonAction:(UIButton *)button {
+    
+    NSString * url =  @"http://phe7wo0ui.bkt.clouddn.com/flutter_assets_grallery.zip";
+
+    NSArray * paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString * path = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"flutter_assets_download/flutter_assets_grallery.zip/flutter_assets"] ;
+    if ([[NSFileManager defaultManager] fileExistsAtPath:path]) {
+        NSURL * url = [NSURL fileURLWithPath:path];
+        FlutterDartProject * dart = [[FlutterDartProject alloc] initWithFlutterAssets:url dartMain:nil packages:nil];
+        FlutterViewController* flutterViewController = [[FlutterViewController alloc] initWithProject:dart nibName:nil bundle:nil];
+        [GeneratedPluginRegistrant registerWithRegistry:flutterViewController];
+
+        [self addBackButton:flutterViewController];
+     
+         [flutterViewController setInitialRoute:@"route1"];
+        [self presentViewController:flutterViewController animated:YES completion:nil];
+    }
+    else{
+    
+        [self get_remote_flutterassets:url progress:^(float progress) {
+            button.enabled = NO;
+            [button setTitle:[NSString stringWithFormat:@"下载中%.2f%%",100*progress] forState:UIControlStateDisabled];
+        } complete:^(NSString *localpath) {
+            button.enabled = YES;
+        }];
+    }
+ 
+    
+}
+
+-(void)addBackButton:(UIViewController *)flutterViewController{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2  * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIButton * btn = [UIButton buttonWithType:UIButtonTypeSystem];
+        [btn setTitle:@"关闭" forState:UIControlStateNormal];
+        btn.frame = CGRectMake(10, 100, 50, 30);
+        [btn addTarget:self action:@selector(buttonTap:) forControlEvents:UIControlEventTouchUpInside];
+        [flutterViewController.view addSubview:btn];
+        self.ctr = flutterViewController;
+    });
+}
+
+-(void)buttonTap:(id)sender{
+//    [self.navigationController popViewControllerAnimated:YES];
+    [self.ctr dismissViewControllerAnimated:YES completion:nil];
+//    [self.ctr clearChannels];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+-(void)get_remote_flutterassets:(NSString *)url progress:(void (^)(float progress))progressblock complete:(void (^)(NSString * localpath))complete{
+  [[SMDHttpEngine sharedInstance] downloadwithurl:url progress:^(CGFloat progress) {
+        NSLog(@"progress:%.2f",progress);
+        progressblock(progress);
+     } error:^(NSError *error, NSString *filePath) {
+        
+//        button.enabled = YES;
+        if (!error) {
+            NSArray * paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+            NSString * path = [[paths objectAtIndex:0] stringByAppendingPathComponent:@"flutter_assets_download"] ;
+            //解压
+            NSString *tempZipDir = [path stringByAppendingPathComponent:url.lastPathComponent];
+            [SSZipArchive unzipFileAtPath:filePath toDestination:tempZipDir progressHandler:nil completionHandler:^(NSString * _Nonnull path, BOOL succeeded, NSError * _Nullable error) {
+                if (!error) {
+                    complete(tempZipDir);
+                }
+            }];
+        }
+    }];
+}
+
+-(UIButton *)createButton:(NSString *)title frame:(CGRect)frame action:(SEL)selector{
+ 
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    [button addTarget:self
+               action:selector
+     forControlEvents:UIControlEventTouchUpInside];
+    [button setTitle:title forState:UIControlStateNormal];
+    UIColor * bgcolor = [UIColor colorWithRed:arc4random()%256/255. green:arc4random()%256/255. blue:arc4random()%256/255. alpha:1];
+    [button setBackgroundColor:bgcolor];
+    button.frame = frame;
+    [self.view addSubview:button];
+    return button;
+}
+
+@end
---
+++ flutterOnExistApp/Resources/Flutter.framework/Headers/FlutterViewController.h
@@ -0,0 +1,72 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef FLUTTER_FLUTTERVIEWCONTROLLER_H_
+#define FLUTTER_FLUTTERVIEWCONTROLLER_H_
+
+#import <UIKit/UIKit.h>
+#include <sys/cdefs.h>
+
+#include "FlutterBinaryMessenger.h"
+#include "FlutterDartProject.h"
+#include "FlutterMacros.h"
+#include "FlutterPlugin.h"
+#include "FlutterTexture.h"
+
+FLUTTER_EXPORT
+@interface FlutterViewController
+    : UIViewController <FlutterBinaryMessenger, FlutterTextureRegistry, FlutterPluginRegistry>
+
+- (instancetype)initWithProject:(FlutterDartProject*)projectOrNil
+                        nibName:(NSString*)nibNameOrNil
+                         bundle:(NSBundle*)nibBundleOrNil NS_DESIGNATED_INITIALIZER;
+
+- (void)handleStatusBarTouches:(UIEvent*)event;
+- (void)clearChannels;
+/**
+ Returns the file name for the given asset.
+ The returned file name can be used to access the asset in the application's main bundle.
+
+ - Parameter asset: The name of the asset. The name can be hierarchical.
+ - Returns: the file name to be used for lookup in the main bundle.
+ */
+- (NSString*)lookupKeyForAsset:(NSString*)asset;
+
+/**
+ Returns the file name for the given asset which originates from the specified package.
+ The returned file name can be used to access the asset in the application's main bundle.
+
+ - Parameters:
+   - asset: The name of the asset. The name can be hierarchical.
+   - package: The name of the package from which the asset originates.
+ - Returns: the file name to be used for lookup in the main bundle.
+ */
+- (NSString*)lookupKeyForAsset:(NSString*)asset fromPackage:(NSString*)package;
+
+/**
+ Sets the first route that the Flutter app shows. The default is "/".
+
+ - Parameter route: The name of the first route to show.
+ */
+- (void)setInitialRoute:(NSString*)route;
+
+- (id<FlutterPluginRegistry>)pluginRegistry;
+
+/**
+ Specifies the view to use as a splash screen. Flutter's rendering is asynchronous, so the first
+ frame rendered by the Flutter application might not immediately appear when the Flutter view is
+ initially placed in the view hierarchy. The splash screen view will be used as a replacement
+ until the first frame is rendered.
+
+ The view used should be appropriate for multiple sizes; an autoresizing mask to have a flexible
+ width and height will be applied automatically.
+
+ If not specified, uses a view generated from `UILaunchStoryboardName` from the main bundle's
+ `Info.plist` file.
+ */
+@property(strong, nonatomic) UIView* splashScreenView;
+
+@end
+
+#endif  // FLUTTER_FLUTTERVIEWCONTROLLER_H_
